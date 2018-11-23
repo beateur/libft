cd618a8c2974e05f5e45178658cf278f615e12eb
Bilel HATTAY <bihattay@e3r13p2.42.fr>
2018-11-23 05:01:00 +0100

    1

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bihattay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 08:20:08 by bihattay          #+#    #+#             */
-/*   Updated: 2018/11/17 07:00:08 by bihattay         ###   ########.fr       */
+/*   Updated: 2018/11/23 05:00:39 by bihattay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,84 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 //#include "get_next_line.h"
+#include "../PROJECTS/libft/libft.h"
 
-void	display_file(const int fd)
+#define BUF_SIZE 4096
+
+int		eroor(char **line, int fd, char **buf)
 {
-	char buf;
+	if (!line || fd < 0)
+		return (0);
+	if (!(*buf = (char *)ft_memalloc(BUFF_SIZE + 1)))
+		return (0);
+	return (1);
+}
 
-	while (read(fd, &buf, 1) != 0)
-		write(1, &buf, 1);
+int		ft_read(int fd, char **ptr, char **buf)
+{
+	int		ret;
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	while ((res = read(fd, *buf, BUF_SIZE)) > 0)
+	{
+		tmp = *buf;
+		tmp[ret] = '\0';
+		tmp = *str;
+		if (*str)
+			*str = ft_strcat(*str, *buf);
+		else
+			*str = ft_strdup(*buf);
+		ft_memdel((void *)&tmp);
+		if (ft_strchr(*str, '\n'))
+		{
+			ft_memdel((void *)buf);
+			return (res);
+		}
+	}
+	ft_memdel((void *)buf);
+	return (res);
+}
+
+int					fillline(char **line, char **str, int *res)
+{
+	int				i;
+	char			*tmp;
+
+	tmp = *str;
+	i = 0;
+	while (tmp[i] != '\0' && tmp[i] != '\n')
+		i++;
+	if (i == 0 || tmp[0] == '\0')
+	{
+		if (!(*line = ft_strdup("")))
+			return (-1);
+	}
+	else if (!(*line = ft_strsub(tmp, 0, i)))
+		return (-1);
+	if (!ft_strlen(*str) && *res == 0)
+		return (0);
+	if (i >= (int)ft_strlen(*str))
+		ft_strclr(*str);
+	else
+		*str = *str + i + 1;
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	char	buf[1];
-	char	*tab;
-	char	*tmp;
+	char		*buf;
+	char		*tmp;
+	char		*ptr[10000] = {NULL};
+	int			res;
+	int			ret;
 
-	if (!(tab = malloc(sizeof(char) * (1))) || (fd == -1))
-		return (0);
-	while ((read(0, buff, 1) > 0))
-	{
-		tmp = tab;
-		if (!(tab = malloc(sizeof(*tab) * (++i + 1))))
-			return (0);
-		tab[0] = '\0';
-		strncat(tab, tmp, i + 1);
-		strncat(tab, buff, 1);
-	}
-	free(tmp);
-	return (0);
+	if (!eroor(line, fd, &buf))
+		return (-1);
+	if ((ret = ft_read(fd, &ptr[fd], &buf)) == -1)
+		return (-1);
+	res = fill(line, &str[fd], &ret);
+	return (res);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bihattay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/15 10:15:54 by bihattay          #+#    #+#             */
-/*   Updated: 2018/11/22 00:35:43 by bihattay         ###   ########.fr       */
+/*   Created: 2018/11/21 23:39:56 by bihattay          #+#    #+#             */
+/*   Updated: 2018/11/21 23:42:30 by bihattay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-//#define BUF_SIZE 1
-int		get_next_line(const int fd, char **line);
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int		main(int argc, char **argv)
-{
-	const int	fd = open(argv[1], O_RDONLY);
+#define BUF_SIZE 4096
 
-	get_next_line(fd, argv);
-	printf("%s", *line);
-	return (0);
-}
+#endif
